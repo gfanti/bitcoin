9afa8e05815127ccdef656f87c0bf655c915e852
Giulia Fanti <giuliafanti@Giulias.local>
2017-06-06 20:54:57 -0500

    made the embargo delay random

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1024,7 +1024,7 @@ void RelayTransactionDandelion(const CTransaction& tx, CConnman& connman) {
         LogPrint(BCLog::NET, "relaying dandelion tx %s to %d\n", hash.ToString(), stemNode->GetId());
         // TODO: exponential time embargo
         int64_t nNow = GetTimeMicros();
-        int64_t embargoTime = nNow + 30 * 1000000; // Embargo lasts 30 seconds
+        int64_t embargoTime = PoissonNextSend(nNow, EMBARGO_MEAN_DELAY) + EMBARGO_FIXED_DELAY * 1000000; 
         embargoTime = std::max(embargoTime, parentEmbargoTime+1);
         auto it = mapEmbargoExpire.insert(std::make_pair(embargoTime, hash));
         CDandelionEmbargo emb = { it, stemNode->GetId() };
